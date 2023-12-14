97270cbe1063890066deea233d5095cb24051cd9
Nguyen Thanh Tai <tainhokhem48@gmail.com>
2023-12-14 21:45:20 +0700

    update test api subjects

--- app/Http/Controllers/Api/SubjectController.php
+++ app/Http/Controllers/Api/SubjectController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Subject;
 use App\Models\Category;
+use App\Models\user;
 use App\Http\Controllers\Api\CategoryController;
 class SubjectController extends Controller
 {
@@ -37,19 +38,21 @@ class SubjectController extends Controller
             }
             // Kiểm tra và xử lý dữ liệu đầu vào
             $validator = Validator::make($request->all(), [
-                'subject_name' => 'required|unique:subjects,name|max:255',
+                
                 'subject_content' => 'required',
                 'subject_image' => 'required',
-                'subject_full_subject'=> 'required|max:255',
-                'subject_price_full' => 'required|numeric'
+                'promotional_price_subject'=> 'required|numeric',
+               
+                
+                'category_id' => 'required|exists:categories,id'
             ], [
-                'subject_name.required' => 'Tên danh mục không được trống',
+               
                 'subject_content.required' => 'Tên content không được trống',
                 'subject_image.required' => 'image không được trống',
-                'subject_full_subject.required' => 'full_subject không được trống',
-                'subject_price_full.required' => 'price_full không được trống',
-                'subject_price_full.numeric' => 'price_full phải là số',
-                'subject_name.unique' => 'Tên danh mục đã tồn tại'
+                'promotional_price_subject.required' => 'promotional_price_subject không được trống',
+                'promotional_price_subject.numeric' => 'promotional_price_subject phai la so',
+                'category_id.exists' => 'Danh mục cha không đúng',
+                'category_id.required' => 'danh mục category_id không được trống '
             ]);
             if ($validator->fails()) {
                 $errors = $validator->errors()->all();
@@ -61,20 +64,22 @@ class SubjectController extends Controller
             }
             $contentValue = $request->input('subject_content', '');
             if ($request->category_id != null) {
-                $subjectParent = Subject::find($request->category_id);
+                $subjectParent = Category::find($request->category_id);
                 if ($subjectParent == null) {
                     return response()->json([
-                        'error_message' => 'Danh mục cha không đúng'
+                        'error_message' => 'Danh mục cha không đúng '
                     ], 400);
                 }
             }
              // Tạo mới một môn học và lưu vào cơ sở dữ liệu
             Subject::create([
-                'name' => $request->subject_name,
+                
                 'content' => $request->subject_content,
                 'image' => $request->subject_image,
-                'full_subject'=> $request->subject_full_subject,
-                'price_full' => $request->subject_price_full,
+                'promotional_price'=> $request->promotional_price_subject,
+                'created_by' => auth()->user()->email,
+                'updated_by' => '',
+                
                 'category_id' => $request->input('category_id', 1)
             ]);
               // Trả về phản hồi JSON với kết quả
@@ -114,20 +119,19 @@ class SubjectController extends Controller
             }
              // Kiểm tra và xử lý dữ liệu đầu vào
             $validator = Validator::make($request->all(), [
-                'subject_name' => 'required|unique:subjects,name|max:255,'.$subject->id,
                 'subject_content' => 'required',
                 'subject_image' => 'required',
-                'subject_full_subject'=> 'required|max:255',
-                'subject_price_full' => 'required|numeric'
+                'promotional_price_subject'=> 'required|numeric',
+                
+                'category_id' => 'required|exists:categories,id'
                 
             ], [
-                'subject_name.required' => 'Tên danh mục không được trống',
                 'subject_content.required' => 'Tên content không được trống',
                 'subject_image.required' => 'image không được trống',
-                'subject_full_subject.required' => 'full_subject không được trống',
-                'subject_price_full.required' => 'price_full không được trống',
-                'subject_price_full.numeric' => 'price_full phải là số',
-                'subject_name.unique' => 'Tên danh mục đã tồn tại'
+                'promotional_price_subject.required' => 'promotional_price_subject không được trống',
+                'promotional_price_subject.numeric' => 'promotional_price_subject phai la so',
+                'category_id.exists' => 'Danh mục cha không đúng',
+                'category_id.required' => 'danh mục category_id không được trống '
             ]);
              // Nếu có lỗi xác minh, trả về thông báo lỗi
             if ($validator->fails()) {
@@ -140,7 +144,7 @@ class SubjectController extends Controller
             }
             // Kiểm tra sự tồn tại của danh mục cha nếu có
             if ($request->category_id != null) {
-                $subjectParent = Subject::find($request->category_id);
+                $subjectParent = Category::find($request->category_id);
                 if ($subjectParent == null) {
                     return response()->json([
                         'error_message' => 'Danh mục cha không đúng'
@@ -151,10 +155,13 @@ class SubjectController extends Controller
             }
             // Cập nhật thông tin môn học và lưu vào cơ sở dữ liệu
             $subject->category_id = $request->input('category_id', 1);
-            
-            $subject->name = $request->subject_name;
-           
+            $subject->content = $request->input('subject_content');
+            $subject->image = $request->input('subject_image');
+            $subject->promotional_price = $request->input('promotional_price_subject');
+            $subject->updated_by = auth()->user()->email;
             $subject->save();
+           
+        
 
 
               // Trả về phản hồi JSON với kết quả
