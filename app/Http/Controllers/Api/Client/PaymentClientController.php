1072f2bd25a923bcc8277b61d8b7371b0203f4d8
Nguyen Thanh Tai <tainhokhem48@gmail.com>
2024-01-05 02:39:26 +0700

    update-Payment

--- app/Http/Controllers/Api/Client/PaymentClientController.php
+++ app/Http/Controllers/Api/Client/PaymentClientController.php
@@ -29,10 +29,12 @@ class PaymentClientController extends Controller
             $validator = Validator::make($request->all(), [
                 'course_id' => 'required',
                 'subject_id' => 'required',
-                'subject_full' => 'required'
+                'subject_full' => 'required',
+                'free' => 'required'
             ], []);
             // Lấy ID của người dùng đã đăng nhập
             $subjectFull = $request->input('subject_full');
+            $free = $request->input('free');
             if ($subjectFull === 1) {
                 $subjectId = $request->input('subject_id');
                 $subject = Subject::with('courses')->find($subjectId);
@@ -80,37 +82,70 @@ class PaymentClientController extends Controller
             } else {
                 $courseId = $request->input('course_id');
                 $course = Course::find($courseId);
-                if ($course === null) {
-                    return response()->json([
-                        'error_message' => 'Không tìm thấy khóa học'
-                    ], 400);
-                } else {
-                    $price = 0;
-                    $price += $course->price;
-                    $price -= $course->promotional_price;
-                    $user = auth()->user();
-                    $payments = Payment::where('users_id', $user->id)->where('courses_id', $course->id)->exists();
+                $user = auth()->user();
+                $price = $course->price - $course->promotional_price;
+                if ($free === 1) {
+                    $payments = Payment::where('users_id', $user->id)->where('courses_id', $courseId)->exists();
                     if ($payments) {
                         return response()->json([
-                            'error_message' => 'Bạn đã mua khóa học và môn học này trước đó.'
+                            'error_message' => 'Bạn đã mua khóa học và môn học này trước đó 11.'
                         ], 400);
                     }
+                    Payment::create([
+                        'price' => 0,
+                        'subject_full' => $subjectFull,
+                        'users_id' => $user->id,
+                        'courses_id' => $course->id
+                    ]);
+                    return response()->json([
+                        'result' => 'success 1'
+                    ], 200);
+                } else {
                     if ($user->money < $price) {
                         return response()->json([
                             'error_message' => 'bạn không đủ tiền trong tài khoản'
                         ], 400);
                     } else {
-                        $user->money -= $price;
-                        $user->save();
-                        Payment::create([
-                            'price' => $price,
-                            'subject_full' => $subjectFull,
-                            'users_id' => $user->id,
-                            'courses_id' => $course->id
-                        ]);
-                        return response()->json([
-                            'result' => 'success'
-                        ], 200);
+                        $payments = Payment::get();
+                        foreach ($payments as $payment) {
+                            if ($payment) {
+                                if ($payment->price > 0) {
+                                    return response()->json([
+                                        'error_message' => 'Bạn đã mua khóa học và môn học này trước đó 22.'
+                                    ], 400);
+                                } else {
+                                    $user->money -= $price;
+                                    $user->save();
+                                    Payment::where('users_id', $user->id)->where('courses_id', $course->id)->update([
+                                        'price' => $price,
+                                        'subject_full' => $subjectFull,
+                                        'users_id' => $user->id,
+                                        'courses_id' => $course->id
+                                    ]);
+                                    return response()->json([
+                                        'price' => 'success 2'
+                                    ], 200);
+                                }
+                            } else {
+                                if ($payment) {
+                                    return response()->json([
+                                        'error_message' => 'Bạn đã mua khóa học và môn học này trước đó 33.'
+                                    ], 400);
+                                } else {
+                                    $user->save();
+                                    $user->money -= $price;
+                                    Payment::create([
+                                        'price' => $price,
+                                        'subject_full' => $subjectFull,
+                                        'users_id' => $user->id,
+                                        'courses_id' => $course->ids
+                                    ]);
+                                    return response()->json([
+                                        'result' => 'success 3'
+                                    ], 200);
+                                }
+                            }
+                        }
                     }
                 }
             }
